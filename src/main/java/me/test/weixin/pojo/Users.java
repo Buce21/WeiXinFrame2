bc6185bad3040341830ac795947f691d70fbe5a5
buce <378741605@qq.com>
2016-02-19 17:16:47 +0800

    登陆成功

---
+++ src/main/java/me/test/weixin/pojo/Users.java
@@ -0,0 +1,43 @@
+package me.test.weixin.pojo;
+
+public class Users {
+    private Integer id;
+
+    private String username;
+
+    private String passworld;
+
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassworld() {
+        return passworld;
+    }
+
+    public void setPassworld(String passworld) {
+        this.passworld = passworld == null ? null : passworld.trim();
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role == null ? null : role.trim();
+    }
+}
